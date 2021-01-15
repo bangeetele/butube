caf8c49b29fb50d07ea563f8a6d09c03c227c6c5
pulasthiad <77054467+pulasthiad@users.noreply.github.com>
2021-01-15 06:45:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "ʀᴇᴘᴏʀᴛ ʙᴜɢs✔️", url="https://t.me/hackingUC")]
     ])
-    welcomed = f"ʜᴇʏ <b>{message.from_user.first_name}</b>\n🔆 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᗰ ʏᴏᴜᴛᴜʙᴇʀ /help ғᴏʀ ᴍᴏʀ ɪɴғᴏ"
+    welcomed = f"🔆 ʜᴇʏ <b>{message.from_user.first_name}</b>\n🔆 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᗰ ʏᴏᴜᴛᴜʙᴇʀ /help ғᴏʀ ᴍᴏʀ ɪɴғᴏ"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
